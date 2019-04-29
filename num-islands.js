7d75b9c2e2d41b355d40458bc701380b0baac2f4
Michael Leddy <31906313+dev-leddy@users.noreply.github.com>
2019-04-29 17:21:20 -0400

    Update num-islands.js

--- num-islands.js
+++ num-islands.js
@@ -1,7 +1,10 @@
-/* Leetcode 200. Number of Islands
+/* 
+Leetcode 200. Number of Islands
 https://leetcode.com/problems/number-of-islands/
 
-Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. 
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
+You may assume all four edges of the grid are all surrounded by water.
 
 Example 1:
 
@@ -12,15 +15,7 @@ Input:
 00000
 
 Output: 1
-Example 2:
 
-Input:
-11000
-11000
-00100
-00011
-
-Output: 3
 */
 
 var numIslands = function(grid) {
